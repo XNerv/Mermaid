186886a63a477263165cd5948aa2172a4c16711c
Thiago da Silva Andrade Soares =^.~= <thiagosilvaandradesoares@gmail.com>
2017-02-13 17:18:00 -0200

    Add sample dock_test2 to reaserch porpuse

---
+++ sources/applications/dock_test2/src/MainComponent.h
@@ -0,0 +1,66 @@
+
+#ifndef MAINCOMPONENT_H_INCLUDED
+#define MAINCOMPONENT_H_INCLUDED
+
+
+#include "juce.h"
+#include "JDockableWindows.h"
+#include "JAdvancedDock.h"
+
+using namespace juce;
+
+
+/**
+A really simple component we can use to test the dragging and docking.
+*/
+class ExampleDockableComponent
+	:
+	public Component
+{
+public:
+	ExampleDockableComponent(const String & componentName, const Colour & colour_)
+		:
+		colour(colour_)
+	{
+		Component::setName(componentName);
+	}
+
+	~ExampleDockableComponent()
+	{
+		jassertfalse;
+	}
+
+	void paint(Graphics & g) override
+	{
+		g.fillAll(colour);
+		g.setColour(Colours::white);
+		g.drawText("Window Content", getLocalBounds(), Justification::centred, false);
+	}
+
+private:
+	Colour colour;
+};
+
+
+
+class MainContentComponent : public Component
+{
+public:
+	//==============================================================================
+	MainContentComponent();
+	~MainContentComponent();
+
+	void paint(Graphics&) override;
+	void resized() override;
+
+private:
+	DockableWindowManager dockManager;
+	WindowDockVertical dock { dockManager };
+	TabDock tabDock { dockManager };
+	JAdvancedDock advancedDock{ dockManager };
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainContentComponent)
+};
+
+
+#endif  // MAINCOMPONENT_H_INCLUDED
